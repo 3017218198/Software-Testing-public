cabaf5d6570876983d8b43b7ae7c67157bac3339
3017218198 <lljlljly@tju.edu.cn>
2020-03-31 16:53:03 +0800

    lab2

---
+++ lab02/src/com/tju/lab2/Lab2.java
@@ -0,0 +1,28 @@
+package com.tju.lab2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
+public class Lab2 {
+
+	public Lab2() {
+		super();
+		// TODO Auto-generated constructor stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		String driverPath = System.getProperty("user.dir") + "/src/chromedriver/chromedriver.exe";
+//		System.setProperty("webdriver.chrome.driver", driverPath);
+//		WebDriver driver = new ChromeDriver();
+//		
+//		String baseUrl = "http://103.120.226.190/selenium-demo/git-repo";
+//		driver.get(baseUrl);
+//		
+//		driver.close();
+	}
+
+}
